3ad44234441f59b36b2090c0aa5c67d91593c9f9
Rocketman <abhinayshambharkar1@gmail.com>
2025-04-10 16:16:04 +0530

    recursion pt 2

---
+++ 02_recursion/006_palindrome_check.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+bool isPal(string &str, int start, int end){
+	if(start>=end) return true;
+	return(str[start]==str[end] && isPal(str,start+1,end-1));
+}
+int main(){
+	string str;
+	cin>>str;
+	int n=str.length();
+	if(isPal(str,0,n-1))cout<<"yes";
+	else cout<<"no";
+}
