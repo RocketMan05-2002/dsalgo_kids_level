3ec4ebb821b40d523ff4075aa39c9563c17071c3
Rocketman <abhinayshambharkar1@gmail.com>
2025-04-13 23:51:08 +0530

    format fixer

--- 02_recursion/006_palindrome_check.cpp
+++ 02_recursion/006_palindrome_check.cpp
@@ -1,13 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-bool isPal(string &str, int start, int end){
-	if(start>=end) return true;
-	return(str[start]==str[end] && isPal(str,start+1,end-1));
+bool isPal(string &str, int start, int end)
+{
+	if (start >= end)
+		return true;
+	return (str[start] == str[end] && isPal(str, start + 1, end - 1));
 }
-int main(){
+int main()
+{
 	string str;
-	cin>>str;
-	int n=str.length();
-	if(isPal(str,0,n-1))cout<<"yes";
-	else cout<<"no";
+	cin >> str;
+	int n = str.length();
+	if (isPal(str, 0, n - 1))
+		cout << "yes";
+	else
+		cout << "no";
 }
--- 02_recursion/007_sum_of_digits.cpp
+++ 02_recursion/007_sum_of_digits.cpp
@@ -1,14 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int sum(int n){
-	if(n==0) return 0;
-	return (n%10)+ sum(n/10);
+int sum(int n)
+{
+	if (n == 0)
+		return 0;
+	return (n % 10) + sum(n / 10);
 }
 
-int main(){
+int main()
+{
 	int n;
-	cin>>n;
+	cin >> n;
 	int ans = sum(n);
-	cout<<ans;
+	cout << ans;
 	return 0;
 }
