1967b23f75bdb1946bcf7346123ecb07c75a68b3
Rocketman <abhinayshambharkar1@gmail.com>
2025-04-13 23:47:12 +0530

    searching pt 2

--- 03_searching/003a_index_first_occurance.cpp
+++ 03_searching/003a_index_first_occurance.cpp
@@ -1,5 +1,20 @@
 #include<iostream>
 using namespace std;
+int FO(int a[], int n, int x){
+    int low=0,high=n-1;
+    int first;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(a[mid]>x) high=mid-1;
+        else if(a[mid]<x) low=mid+1;
+        else{
+            first=mid;
+            break;
+        }
+    }
+    while(first>0 && a[first-1]== a[first]) first--;
+    return first;
+}
 int main(){
     int arr[]={10,20,20,20,30,30,40,40};
     int n= sizeof(arr)/sizeof(arr[0]);
