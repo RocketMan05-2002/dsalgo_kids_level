5f5baee65a0922d6358baf5415e760bd23c263b9
Rocketman <abhinayshambharkar1@gmail.com>
2025-04-13 23:47:59 +0530

    format fixes

--- 03_searching/003a_index_first_occurance.cpp
+++ 03_searching/003a_index_first_occurance.cpp
@@ -1,26 +1,33 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int FO(int a[], int n, int x){
-    int low=0,high=n-1;
+int FO(int a[], int n, int x)
+{
+    int low = 0, high = n - 1;
     int first;
-    while(low<=high){
-        int mid=(low+high)/2;
-        if(a[mid]>x) high=mid-1;
-        else if(a[mid]<x) low=mid+1;
-        else{
-            first=mid;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (a[mid] > x)
+            high = mid - 1;
+        else if (a[mid] < x)
+            low = mid + 1;
+        else
+        {
+            first = mid;
             break;
         }
     }
-    while(first>0 && a[first-1]== a[first]) first--;
+    while (first > 0 && a[first - 1] == a[first])
+        first--;
     return first;
 }
-int main(){
-    int arr[]={10,20,20,20,30,30,40,40};
-    int n= sizeof(arr)/sizeof(arr[0]);
+int main()
+{
+    int arr[] = {10, 20, 20, 20, 30, 30, 40, 40};
+    int n = sizeof(arr) / sizeof(arr[0]);
     int x;
-    cin>>x;
-    int ind = FO(arr,n,x);
-    cout<<ind;
+    cin >> x;
+    int ind = FO(arr, n, x);
+    cout << ind;
     return 0;
 }
