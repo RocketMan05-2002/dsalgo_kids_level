989bd81b4a403a894280e1df62877451b32958f2
Rocketman <abhinayshambharkar1@gmail.com>
2025-04-17 23:51:25 +0530

    sorting3

--- 04_sorting/003_insertion_sort.cpp
+++ 04_sorting/003_insertion_sort.cpp
@@ -1,20 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-void insertionSort(int arr[], int n){
-    for(int i=0;i<n;i++){
-        int j=i;
-        while(j>0 && arr[j-1]>arr[j]){
-            swap(arr[j-1],arr[j]);
+void insertionSort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
+            swap(arr[j - 1], arr[j]);
             j--;
         }
     }
 }
-int main(){
-    int arr[]={5,1,4,3,2};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    for(auto it:arr) cout<<it<<" ";
-    cout<<endl;
-    insertionSort(arr,n);
-    for(auto it:arr) cout<<it<<" ";
+int main()
+{
+    int arr[] = {5, 1, 4, 3, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for (auto it : arr)
+        cout << it << " ";
+    cout << endl;
+    insertionSort(arr, n);
+    for (auto it : arr)
+        cout << it << " ";
     return 0;
 }
