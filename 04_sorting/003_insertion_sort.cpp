d0446b3ea8b56bf17a913574b4c590e04390fc1c
Rocketman <abhinayshambharkar1@gmail.com>
2025-04-17 23:50:39 +0530

    sorting

---
+++ 04_sorting/003_insertion_sort.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void insertionSort(int arr[], int n){
+    for(int i=0;i<n;i++){
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j]){
+            swap(arr[j-1],arr[j]);
+            j--;
+        }
+    }
+}
+int main(){
+    int arr[]={5,1,4,3,2};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(auto it:arr) cout<<it<<" ";
+    cout<<endl;
+    insertionSort(arr,n);
+    for(auto it:arr) cout<<it<<" ";
+    return 0;
+}
---
+++ 04_sorting/005_merge_sort.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+void merge(int arr[], int low, int high){
+
+}
+void mS(int arr[], int low, int high){
+    if(low==high) return;
+    int mid=(low+high)/2;
+    mS(arr,low,mid);
+    mS(arr,mid+1,high);
+    merge(arr,low,mid,high);
+}
+void mergeSort(int arr[], int n){
+    mS(arr,0,n-1);
+}
+int main(){
+    int arr[]={5,1,4,3,2};
+    int n=5;
+    for(auto it:arr) cout<<it<<" ";
+    cout<<endl;
+    mergeSort(arr,n);
+    for(auto it:arr) cout<<it<<" ";
+    return 0;
+}
