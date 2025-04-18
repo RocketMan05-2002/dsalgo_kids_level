2b5fb885e573015b7ab7032cbd7e82631564274f
Rocketman <abhinayshambharkar1@gmail.com>
2025-04-18 23:10:27 +0530

    sorting3

--- 03_searching/008_square_root.cpp
+++ 03_searching/008_square_root.cpp
@@ -1,24 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int sqroot(int x){
-    int low=0,high=x;
+int sqroot(int x)
+{
+    int low = 0, high = x;
     int ans = -1;
-    while(low<=high){
-        int mid = (low+high)/2;
-        int msq = mid*mid;
-        if(msq == x) return mid;
-        else if(msq > x) high = mid-1;
-        else{
-            low=mid+1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int msq = mid * mid;
+        if (msq == x)
+            return mid;
+        else if (msq > x)
+            high = mid - 1;
+        else
+        {
+            low = mid + 1;
             ans = mid;
         }
     }
     return ans;
 }
-int main(){
+int main()
+{
     int x;
-    cin>>x;
+    cin >> x;
     int ans = sqroot(x);
-    cout<<ans;
+    cout << ans;
     return 0;
 }
