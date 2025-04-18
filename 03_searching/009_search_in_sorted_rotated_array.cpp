703829a707ec95cd2ecf6c8c0cfdf504af5dec60
Rocketman <abhinayshambharkar1@gmail.com>
2025-04-18 23:10:07 +0530

    sorting3

--- 03_searching/009_search_in_sorted_rotated_array.cpp
+++ 03_searching/009_search_in_sorted_rotated_array.cpp
@@ -1,28 +1,39 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int search(int arr[],int n, int x){
-    int low=0,high= n-1;
-    while(low<=high){
-        int mid = (low+high)/2;
-        if(arr[mid]==x) return mid;
-        if(arr[low]<=arr[mid]){
-            //left half is sorted check the range
-            if(x>=arr[low] && x<arr[mid]) high=mid-1;
-            else low=mid+1;
-        }else{
-            //right half is sorted
-            if(x>arr[mid]&& x<=arr[high]) low=mid+1;
-            else high=mid-1;
+int search(int arr[], int n, int x)
+{
+    int low = 0, high = n - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == x)
+            return mid;
+        if (arr[low] <= arr[mid])
+        {
+            // left half is sorted check the range
+            if (x >= arr[low] && x < arr[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        else
+        {
+            // right half is sorted
+            if (x > arr[mid] && x <= arr[high])
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
     }
     return -1;
 }
-int main(){
-    int arr[]={10,20,40,60,6,8};
-    int n =sizeof(arr)/sizeof(arr[0]);
+int main()
+{
+    int arr[] = {10, 20, 40, 60, 6, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
     int x;
-    cin>>x;
-    int ind = search(arr,n,x);
-    cout<<ind;
+    cin >> x;
+    int ind = search(arr, n, x);
+    cout << ind;
     return 0;
 }
