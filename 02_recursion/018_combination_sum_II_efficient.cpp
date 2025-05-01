1005988d4f3144023f03a6001405e7b906242d0a
Rocketman <abhinayshambharkar1@gmail.com>
2025-05-01 23:50:46 +0530

    recursion push 5

--- 02_recursion/018_combination_sum_II_efficient.cpp
+++ 02_recursion/018_combination_sum_II_efficient.cpp
@@ -1,32 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void fun(int ind, int target, vector<int> &ds, vector<int> &arr, vector<vector<int>> &ans){
-    if(target==0){
+void fun(int ind, int target, vector<int> &ds, vector<int> &arr, vector<vector<int>> &ans)
+{
+    if (target == 0)
+    {
         ans.push_back(ds);
         return;
     }
-    for(int i= ind;i<arr.size();i++){
-        if(i>ind && arr[i-1]==arr[i]) continue;
-        if(arr[i]>target) break;
+    for (int i = ind; i < arr.size(); i++)
+    {
+        if (i > ind && arr[i - 1] == arr[i])
+            continue;
+        if (arr[i] > target)
+            break;
         ds.push_back(arr[i]);
-        fun(i+1,target-arr[i],ds,arr,ans);
+        fun(i + 1, target - arr[i], ds, arr, ans);
         ds.pop_back();
     }
 }
-vector<vector<int>> findCombos(vector<int> &vec, int target){
+vector<vector<int>> findCombos(vector<int> &vec, int target)
+{
     vector<vector<int>> ans;
     vector<int> ds;
-    fun(0,target,ds,vec,ans);
+    fun(0, target, ds, vec, ans);
     return ans;
 }
-int main(){
-    vector<int>vec= {1,1,1,2,2};
-    int target =4;
-    vector<vector<int>> ans =findCombos(vec,target);
-    for(auto it:ans){
-        for(auto ele:it){
-            cout<<ele<<" ";
+int main()
+{
+    vector<int> vec = {1, 1, 1, 2, 2};
+    int target = 4;
+    vector<vector<int>> ans = findCombos(vec, target);
+    for (auto it : ans)
+    {
+        for (auto ele : it)
+        {
+            cout << ele << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 }
