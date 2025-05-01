a66aaafa5afcaefdbbbcd5247aaef1f0e13990ee
Rocketman <abhinayshambharkar1@gmail.com>
2025-05-01 23:49:49 +0530

    recursion push 5

--- 02_recursion/022_permutations_II.cpp
+++ 02_recursion/022_permutations_II.cpp
@@ -1,33 +1,40 @@
 // an efficient approach
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void fun(int ind, vector<int> &nums, vector<vector<int>> &ans){
+void fun(int ind, vector<int> &nums, vector<vector<int>> &ans)
+{
 
-    if(ind == nums.size()){
+    if (ind == nums.size())
+    {
         ans.push_back(nums);
         return;
     }
 
-    for(int i=ind; i<nums.size();i++){
-        swap(nums[ind],nums[i]);
-        fun(ind+1,nums,ans);
-        swap(nums[ind],nums[i]);
+    for (int i = ind; i < nums.size(); i++)
+    {
+        swap(nums[ind], nums[i]);
+        fun(ind + 1, nums, ans);
+        swap(nums[ind], nums[i]);
     }
 }
-vector<vector<int>> permutations(vector<int> &nums){
+vector<vector<int>> permutations(vector<int> &nums)
+{
     vector<vector<int>> ans;
-    fun(0,nums,ans);
+    fun(0, nums, ans);
     return ans;
 }
-int main(){
-    vector<int> nums = {1,2,3};
+int main()
+{
+    vector<int> nums = {1, 2, 3};
     vector<vector<int>> ans = permutations(nums);
-    for(auto it:ans){
-        for(auto ele:it){
-            cout<<ele<<" ";
+    for (auto it : ans)
+    {
+        for (auto ele : it)
+        {
+            cout << ele << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
