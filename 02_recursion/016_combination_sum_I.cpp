329d4a82373d2f3918588e614c632d0f7c960fa5
Rocketman <abhinayshambharkar1@gmail.com>
2025-05-01 23:51:13 +0530

    recursion push 5

--- 02_recursion/016_combination_sum_I.cpp
+++ 02_recursion/016_combination_sum_I.cpp
@@ -1,38 +1,46 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void fun(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds){
-    
-    if(ind==arr.size()){
-        if(target==0){
+void fun(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds)
+{
+
+    if (ind == arr.size())
+    {
+        if (target == 0)
+        {
             ans.push_back(ds);
         }
         return;
     }
 
-    //pick
-    if(arr[ind]<=target){
+    // pick
+    if (arr[ind] <= target)
+    {
         ds.push_back(arr[ind]);
-        fun(ind,target-arr[ind],arr,ans,ds);
+        fun(ind, target - arr[ind], arr, ans, ds);
         ds.pop_back();
     }
-    //not pick
-    fun(ind+1,target,arr,ans,ds);
+    // not pick
+    fun(ind + 1, target, arr, ans, ds);
 }
-vector<vector<int>> F(vector<int> &arr, int target){
-    vector<int>ds;
+vector<vector<int>> F(vector<int> &arr, int target)
+{
+    vector<int> ds;
     vector<vector<int>> ans;
-    fun(0,target,arr,ans,ds);
+    fun(0, target, arr, ans, ds);
     return ans;
 }
-int main(){
-    vector<int>vec= {2,3,6,7};
+int main()
+{
+    vector<int> vec = {2, 3, 6, 7};
     int target = 7;
-    vector<vector<int>> ans = F(vec,target);
-    for(auto it:ans){
-        for(auto ele:it){
-            cout<<ele<<" ";
+    vector<vector<int>> ans = F(vec, target);
+    for (auto it : ans)
+    {
+        for (auto ele : it)
+        {
+            cout << ele << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
