746cf4c3f26662bc06d6a910381ccc968322221c
Rocketman <abhinayshambharkar1@gmail.com>
2025-05-01 23:50:22 +0530

    recursion push 5

--- 02_recursion/020_subsets_II.cpp
+++ 02_recursion/020_subsets_II.cpp
@@ -1,29 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void fun(int ind, vector<int> &ds, vector<int> &arr, vector<vector<int>> &ans){
+void fun(int ind, vector<int> &ds, vector<int> &arr, vector<vector<int>> &ans)
+{
     ans.push_back(ds);
-    for(int i=ind;i<arr.size();i++){
-        if(i!=ind && arr[i-1]==arr[i]) continue;
+    for (int i = ind; i < arr.size(); i++)
+    {
+        if (i != ind && arr[i - 1] == arr[i])
+            continue;
         ds.push_back(arr[i]);
-        fun(i+1,ds,arr,ans);
+        fun(i + 1, ds, arr, ans);
         ds.pop_back();
     }
 }
-vector<vector<int>> fun(vector<int> &arr){
+vector<vector<int>> fun(vector<int> &arr)
+{
     vector<vector<int>> ans;
     vector<int> ds;
-    sort(arr.begin(),arr.end());
-    fun(0,ds,arr,ans);
+    sort(arr.begin(), arr.end());
+    fun(0, ds, arr, ans);
     return ans;
 }
-int main(){
-    vector<int> arr={1,2,2};
+int main()
+{
+    vector<int> arr = {1, 2, 2};
     vector<vector<int>> ans = fun(arr);
-    for(auto it:ans){
-    	for(auto ele:it){
-    		cout<<ele<<" ";
-    	}
-    	cout<<endl;
+    for (auto it : ans)
+    {
+        for (auto ele : it)
+        {
+            cout << ele << " ";
+        }
+        cout << endl;
     }
     return 0;
 }
