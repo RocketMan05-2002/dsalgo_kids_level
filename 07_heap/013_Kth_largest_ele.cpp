58dc59fadb6e29851ef7d4d07611fe9f364dd885
Rocketman <abhinayshambharkar1@gmail.com>
2025-05-08 23:50:16 +0530

    heap push 3

--- 07_heap/013_Kth_largest_ele.cpp
+++ 07_heap/013_Kth_largest_ele.cpp
@@ -1,50 +1,63 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void max_heapify(int arr[], int n, int i){
-    int L = 2*i+1;
-    int R = 2*i+2;
+void max_heapify(int arr[], int n, int i)
+{
+    int L = 2 * i + 1;
+    int R = 2 * i + 2;
     int largest;
-    if(L<n && arr[L]>arr[i]) largest = L;
-    else largest =i;
-    if(R<n && arr[R]>arr[largest]) largest = R;
-    if(largest != i){
-        swap(arr[i],arr[largest]);
-        max_heapify(arr,n,largest);
+    if (L < n && arr[L] > arr[i])
+        largest = L;
+    else
+        largest = i;
+    if (R < n && arr[R] > arr[largest])
+        largest = R;
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+        max_heapify(arr, n, largest);
     }
 }
-void build_max_heap(int arr[], int n){
-    for(int i=floor(n/2)-1;i>=0;i--){
-        max_heapify(arr,n,i);
+void build_max_heap(int arr[], int n)
+{
+    for (int i = floor(n / 2) - 1; i >= 0; i--)
+    {
+        max_heapify(arr, n, i);
     }
 }
-int heap_pop(int arr[], int &n){
-    if(n<1){
-        cout<<"Heap underflow";
+int heap_pop(int arr[], int &n)
+{
+    if (n < 1)
+    {
+        cout << "Heap underflow";
         return -1;
     }
     int maxi = arr[0];
-    arr[0]=arr[n-1];
-    n=n-1;
-    max_heapify(arr,n,0);
+    arr[0] = arr[n - 1];
+    n = n - 1;
+    max_heapify(arr, n, 0);
     return maxi;
 }
-int kthLargest(int arr[], int n, int k){
+int kthLargest(int arr[], int n, int k)
+{
     // convert the arr into max heap
     // heap op k times, kth time you gonna get what you need.
-    if(k>n) k= k%n;
-    build_max_heap(arr,n);
+    if (k > n)
+        k = k % n;
+    build_max_heap(arr, n);
     int ans;
-    for(int i=0;i<k;i++){
-        ans = heap_pop(arr,n);
+    for (int i = 0; i < k; i++)
+    {
+        ans = heap_pop(arr, n);
     }
     return ans;
 }
-int main(){
-    int arr[]={8,1,3,2,6,7};
-    int n = sizeof(arr)/sizeof(arr[0]);
+int main()
+{
+    int arr[] = {8, 1, 3, 2, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
     int k;
-    cin>>k;
-    int ans = kthLargest(arr,n,k);
-    cout<<ans<<endl;
+    cin >> k;
+    int ans = kthLargest(arr, n, k);
+    cout << ans << endl;
     return 0;
 }
