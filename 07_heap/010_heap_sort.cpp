5206954c64d43e027631d8967018c3f8ec31dda1
Rocketman <abhinayshambharkar1@gmail.com>
2025-05-08 23:50:47 +0530

    heap push 3

--- 07_heap/010_heap_sort.cpp
+++ 07_heap/010_heap_sort.cpp
@@ -1,42 +1,54 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void max_heapify(int arr[], int n, int i){
-    int L = 2*i+1;
-    int R = 2*i+2;
+void max_heapify(int arr[], int n, int i)
+{
+    int L = 2 * i + 1;
+    int R = 2 * i + 2;
     int largest;
-    if(L<n && arr[L]>arr[i]){
+    if (L < n && arr[L] > arr[i])
+    {
         largest = L;
-    }else{
+    }
+    else
+    {
         largest = i;
     }
-    if(R<n && arr[R]>arr[largest]){
+    if (R < n && arr[R] > arr[largest])
+    {
         largest = R;
     }
-    if(largest!=i){
-        swap(arr[i],arr[largest]);
-        max_heapify(arr,n,largest);
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+        max_heapify(arr, n, largest);
     }
 }
-void build_max_heap(int arr[], int n){
-    for(int i=floor(n/2)-1;i>=0;i--){
-        max_heapify(arr,n,i);
+void build_max_heap(int arr[], int n)
+{
+    for (int i = floor(n / 2) - 1; i >= 0; i--)
+    {
+        max_heapify(arr, n, i);
     }
 }
-void heapSort(int arr[], int n){
+void heapSort(int arr[], int n)
+{
     // build max heap first.
-    build_max_heap(arr,n);
-    for(int i=n-1;i>0;i--){
-        swap(arr[i],arr[0]);
-        max_heapify(arr,i,0);
+    build_max_heap(arr, n);
+    for (int i = n - 1; i > 0; i--)
+    {
+        swap(arr[i], arr[0]);
+        max_heapify(arr, i, 0);
     }
-
 }
-int main(){
-    int arr[]={5,1,4,3,2,6,7,8};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    for(auto it:arr) cout<<it<<" ";
-    cout<<endl;
-    heapSort(arr,n);
-    for(auto it:arr) cout<<it<<" ";
+int main()
+{
+    int arr[] = {5, 1, 4, 3, 2, 6, 7, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for (auto it : arr)
+        cout << it << " ";
+    cout << endl;
+    heapSort(arr, n);
+    for (auto it : arr)
+        cout << it << " ";
     return 0;
 }
