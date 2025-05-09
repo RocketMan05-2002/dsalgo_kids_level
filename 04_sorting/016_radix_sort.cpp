f8377e516f33eb313405f5c12bc78647e843a404
Rocketman <abhinayshambharkar1@gmail.com>
2025-05-09 23:47:21 +0530

    sorting push 5

--- 04_sorting/016_radix_sort.cpp
+++ 04_sorting/016_radix_sort.cpp
@@ -5,42 +5,52 @@
 // so, TC = O(n). VERY EFFICIENT
 // SC = O(n). (the ans and freq array we'd use every itereation would be of order n)
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void countSort(int arr[], int n, int pos){
-    //frequency array
-    int freq[10] ={0};
-    for(int i=0;i<n;i++){
-        freq[(arr[i]/pos)%10]++;
+void countSort(int arr[], int n, int pos)
+{
+    // frequency array
+    int freq[10] = {0};
+    for (int i = 0; i < n; i++)
+    {
+        freq[(arr[i] / pos) % 10]++;
     }
-    //cumulative freq array
-    for(int i=1;i<=9;i++){
-        freq[i] += freq[i-1];
+    // cumulative freq array
+    for (int i = 1; i <= 9; i++)
+    {
+        freq[i] += freq[i - 1];
     }
-    //ans array
+    // ans array
     int ans[n];
-    for(int i=n-1;i>=0;i--){
-        ans[--freq[(arr[i]/pos)%10]]=arr[i];
+    for (int i = n - 1; i >= 0; i--)
+    {
+        ans[--freq[(arr[i] / pos) % 10]] = arr[i];
     }
 
-    //ans -> arr
-    for(int i=0;i<n;i++){
-        arr[i]=ans[i];
+    // ans -> arr
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = ans[i];
     }
 }
-void radixSort(int arr[], int n){
+void radixSort(int arr[], int n)
+{
     int maxi = INT_MIN;
-    for(int i=0;i<n;i++){
-        maxi = max(maxi,arr[i]);
+    for (int i = 0; i < n; i++)
+    {
+        maxi = max(maxi, arr[i]);
     }
-    for(int pos = 1; maxi/pos>0 ; pos *= 10){
-        countSort(arr,n,pos);
+    for (int pos = 1; maxi / pos > 0; pos *= 10)
+    {
+        countSort(arr, n, pos);
     }
 }
-int main(){
-    int arr[]={190,45,75,90,802,2};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    radixSort(arr,n);
-    for(auto it:arr) cout<<it<<" ";
+int main()
+{
+    int arr[] = {190, 45, 75, 90, 802, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    radixSort(arr, n);
+    for (auto it : arr)
+        cout << it << " ";
     return 0;
 }
