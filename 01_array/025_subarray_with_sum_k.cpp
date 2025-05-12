24435c70a28f5e7ae9d2b0d6026a42f496e2178e
Rocketman <abhinayshambharkar1@gmail.com>
2025-05-12 23:57:14 +0530

    array push 6

--- 01_array/025_subarray_with_sum_k.cpp
+++ 01_array/025_subarray_with_sum_k.cpp
@@ -1,14 +1,17 @@
-int countSubarrays(vector<int> &arr, int k) {
+int countSubarrays(vector<int> &arr, int k)
+{
     // code here
-    unordered_map<int,int>prefix;
-    int sum =0;
+    unordered_map<int, int> prefix;
+    int sum = 0;
     int count = 0;
-    prefix[0]=1;
-    for(int i=0;i<arr.size();i++){
+    prefix[0] = 1;
+    for (int i = 0; i < arr.size(); i++)
+    {
         sum += arr[i];
-        if(prefix.find(sum-k)!=prefix.end()){
+        if (prefix.find(sum - k) != prefix.end())
+        {
             // means its present
-            count += prefix[sum-k];
+            count += prefix[sum - k];
         }
         prefix[sum]++;
     }
