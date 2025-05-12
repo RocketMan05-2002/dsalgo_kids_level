f5474da10de46ffad88335ee28d3aa4218ae7b6f
Rocketman <abhinayshambharkar1@gmail.com>
2025-05-12 23:58:26 +0530

    array push 6

--- 01_array/023_peak_element.cpp
+++ 01_array/023_peak_element.cpp
@@ -1,18 +1,12 @@
-int peakElement(vector<int> &arr)
-{
+int peakElement(vector<int> &arr) {
     // Your code here
-    int n = arr.size();
-    if (n == 1)
-        return 0;
-    if (arr[0] > arr[1])
-        return 0;
-    if (arr[n - 1] > arr[n - 2])
-        return n - 1;
-
-    for (int i = 1; i < n; i++)
-    {
-        if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1])
-            return i;
+    int n=arr.size();
+    if(n==1) return 0;
+    if(arr[0]>arr[1]) return 0;
+    if(arr[n-1]>arr[n-2]) return n-1;
+    
+    for(int i=1;i<n;i++){
+        if(arr[i]>arr[i-1] && arr[i]>arr[i+1]) return i;
     }
     return -1;
 }
--- 01_array/024_3_sum_closest.cpp
+++ 01_array/024_3_sum_closest.cpp
@@ -1,33 +1,27 @@
-int closest3Sum(vector<int> &arr, int target)
-{
+int closest3Sum(vector<int> &arr, int target) {
     // code here
     // sort the array
-    int n = arr.size();
-    sort(arr.begin(), arr.end());
+    int n=arr.size();
+    sort(arr.begin(),arr.end());
     int closestSum = INT_MAX;
     int minDiff = INT_MAX;
-
-    for (int i = 0; i < n - 2; i++)
-    {
-        int left = i + 1;
-        int right = n - 1;
-        while (left < right)
-        {
-            int sum = arr[i] + arr[left] + arr[right];
-            int diff = abs(sum - target);
-
-            if (diff < minDiff || (diff == minDiff && sum > closestSum))
-            {
+    
+    for(int i=0;i<n-2;i++){
+        int left = i+1;
+        int right = n-1;
+        while(left<right){
+            int sum = arr[i]+arr[left]+arr[right];
+            int diff = abs(sum-target);
+            
+            if(diff<minDiff || (diff==minDiff && sum>closestSum)){
                 closestSum = sum;
                 minDiff = diff;
             }
-
-            if (sum < target)
-                left++;
-            else
-                right--;
+            
+            if(sum<target) left++;
+            else right--;
         }
     }
-
+    
     return closestSum;
 }
