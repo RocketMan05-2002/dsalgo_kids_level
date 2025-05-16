05d7e9ad21e3a6a0d0477ef1d533f04ad5e89ab4
Rocketman <abhinayshambharkar1@gmail.com>
2025-05-16 12:32:20 +0530

    recursion push 5

--- 02_recursion/012_print_subseq_with_sum_k.cpp
+++ 02_recursion/012_print_subseq_with_sum_k.cpp
@@ -1,24 +1,36 @@
+// print all subsequences with sum k
 #include<bits/stdc++.h>
 using namespace std;
-void fun(int ind, vector<int> &curr, vector<int> &vec, int sum){
-    if(ind==vec.size() && sum==0){
-        for(auto it:curr) cout<<it<<" ";
-        cout<<endl;
-        return;
-    }
-    curr.push_back(vec[ind]);
-    fun(ind+1,curr,vec,sum-vec[ind]);
-    curr.pop_back();
-    fun(ind+1,curr,vec,sum);
+void fun(int ind, vector<int> &ds, int sum, vector<int> &arr, vector<vector<int>> &ans){
+	if(ind==arr.size()){
+		if(sum==0){
+			ans.push_back(ds);
+		}
+		return;
+	}
+	//pick
+	ds.push_back(arr[ind]);
+	fun(ind+1,ds,sum-arr[ind],arr,ans);
+	ds.pop_back();
+	//not pick
+	fun(ind+1,ds,sum,arr,ans);
 }
-void F(vector<int> &vec, int k){
-    vector<int> &curr;
-    fun(0,curr,vec,k);
+vector<vector<int>> subsKsum(vector<int> &arr, int k){
+	vector<vector<int>> ans;
+	vector<int> ds;
+	fun(0,ds,k,arr,ans);
+	return ans;
 }
 int main(){
-    vector<int>vec={1,2,1};
-    int k;
-    cin>>k;
-    F(vec,k);
-    return 0;
+	vector<int>arr{1,2,1};
+	int k;
+	cin>>k;
+	vector<vector<int>> ans = subsKsum(arr,k);
+	for(auto it:ans){
+		for(auto ele:it){
+			cout<<ele<<" ";
+		}
+		cout<<endl;
+	}
+	return 0;
 }
